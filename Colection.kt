81809eb706e07538f3d2c3a698eeb4c40b3c92c2
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-10-13 14:11:43 +0700

    1

---
+++ Colection.kt
@@ -0,0 +1,28 @@
+class Colection {
+    fun ContohList() {
+        println("== List ==")
+        // List Read-Only
+        val readOnlyAbjad = listOf("A", "B", "C")
+        println(readOnlyAbjad)
+
+        // List Mutable
+        val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+        println(shape)
+
+        // Menambahkan data ke dalam List Mutable
+        shape.add("Circle")
+        println(shape)
+
+        shape.remove("Triangle")
+        println(shape)
+
+        println(shape.first())
+        println(shape.last())
+        println(shape.count())
+
+        shape.removeAt(1)
+        println(shape)
+
+        // Mengubah data didalam List Mutable
+        shape[0]= "Oval"
+        println(shape)
