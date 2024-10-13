1e687ea143edcd1e6f45b1368341f01b3eb50ba7
Nurachmad Luthfi Rizaldy <nurachmadluthfi83@gmail.com>
2024-10-13 15:28:18 +0700

    fun

---
+++ Functions.kt
@@ -0,0 +1,11 @@
+package com.example.test
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
